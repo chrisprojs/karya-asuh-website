4f87994eb5335ecbfd031bbe661c95cbf4018ddc
chrisprojs <christiananggaresta20@gmail.com>
2024-12-26 21:41:03 +0700

    Fix:Deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,22 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-$app->useStoragePath(env('APP_STORAGE', base_path() . '/storage'));
-
-// Prevent caching of the package manifest
-$app->singleton(
-    Illuminate\Foundation\PackageManifest::class,
-    function ($app) {
-        return new Illuminate\Foundation\PackageManifest(
-            new Illuminate\Filesystem\Filesystem,
-            $app->basePath(),
-            $app->basePath('vendor/composer/installed.json')
-        );
-    }
-);
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
